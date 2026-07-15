e5977c8648a622518e503c5cebb53223541a5a4d
Ofek Ben-Yaish <obenyaish@gmail.com>
2026-07-16 01:53:26 +0300

    Add CTA icons

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 export default defineConfig({
   site: "https://ofekby.github.io",
   base: "/modal-uv",
   output: "static",
+  integrations: [icon()],
 });
